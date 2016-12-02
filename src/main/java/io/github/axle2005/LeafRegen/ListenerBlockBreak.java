42d96d80832668a29043d7ab35968da56d96a3eb
arnol <arnol@Axle-Laptop>
2016-12-02 12:57:14 -0400

    Check against BlockTypes instead of Strings.

--- src/main/java/io/github/axle2005/LeafRegen/ListenerBlockBreak.java
+++ src/main/java/io/github/axle2005/LeafRegen/ListenerBlockBreak.java
@@ -2,9 +2,12 @@ package io.github.axle2005.LeafRegen;
 
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockSnapshot;
+import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.event.EventListener;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.filter.type.Include;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.scheduler.Scheduler;
 import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.world.BlockChangeFlag;
@@ -28,11 +31,10 @@ public class ListenerBlockBreak implements EventListener<ChangeBlockEvent> {
 			
 		if (!(event.getTransactions().isEmpty())) {
 			BlockSnapshot leaf = event.getTransactions().get(0).getOriginal();
-			String block = leaf.getState().getType().getId();
-			if (block.equals("minecraft:leaves") || block.equals("minecraft:log")) {
+			if (leaf.getState().getType().equals(BlockTypes.LOG) || leaf.getState().getType().equals(BlockTypes.LEAVES)) {
 				if(!(leaf.getCreator().isPresent()))
 				{
-			restore = taskBuilder.execute(() -> restore(leaf)).async().delayTicks(24000).submit(plugin);
+			restore = taskBuilder.execute(() -> restore(leaf)).async().delayTicks(100).submit(plugin);
 				}
 			}
 		}
